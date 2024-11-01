f47e5983eae30e90a0f39600e52ea65a05cef4a9
Najdorf12 <agustin.morro@gmail.com>
2024-11-01 15:12:55 -0300

    fix

--- index.js
+++ index.js
@@ -8,17 +8,13 @@ dotenv.config();
 const app = express();
 const resend = new Resend("re_SjeR9Xmy_HaZJDE7WtitVrVWvhvuWjSgD");
 
-// Middleware de CORS ajustado manualmente
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://www.serviciotecnicoinfinix.com.ar");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    if (req.method === "OPTIONS") {
-        return res.status(204).end();
-    }
-    next();
-});
+// Middleware de CORS
+app.use(cors({
+    origin: "https://www.serviciotecnicoinfinix.com.ar",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}));
 
 app.use(express.json());
 
@@ -27,22 +23,25 @@ app.post('/send-email', async (req, res) => {
     const { email, wttp, message } = req.body;
 
     try {
-        const response = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>', // Cambia a tu dominio autenticado Acme <onboarding@resend.dev>
-            to: ['info@serviciotecnicoxiaomi.com.ar'],
-            subject: 'Nuevo mensaje de contacto',
+        const { data, error } = await resend.emails.send({
+            from: "Acme <onboarding@resend.dev>", 
+            to: ["info@serviciotecnicoxiaomi.com.ar"], 
+            subject: `Consulta de ${email}.`,
             html: `
-                <h1>Detalles del mensaje:</h1>
+                <h1>Detalles del contacto</h1>
                 <p><strong>Email:</strong> ${email}</p>
                 <p><strong>WhatsApp:</strong> ${wttp}</p>
                 <p><strong>Mensaje:</strong> ${message}</p>
             `,
         });
 
-        res.status(200).json({ message: 'Correo enviado con éxito', data: response });
-    } catch (error) {
-        console.error('Error al enviar el correo:', error);
-        res.status(500).json({ message: 'Error al enviar el correo', error: error.message });
+        if (error) {
+            return res.status(500).json({ error: "Error al enviar el correo" });
+        }
+        res.status(200).json({ message: "Correo enviado exitosamente", data });
+    } catch (err) {
+        console.error("Error en el controlador:", err);
+        res.status(500).json({ error: "Error interno del servidor" });
     }
 });
 
